ba41645c59c1229705af2d6fc5b8ce3a5a2d52ce
Eduardo Escoto <eduardo@escoto.me>
2018-02-02 22:01:12 -0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Store/store';
+import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
